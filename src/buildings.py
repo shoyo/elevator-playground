54ec255bf424a4357aab30c2a63504461bf2760b
shoyo-inokuchi <shoyoinokuchi@gmail.com>
2018-11-26 11:08:10 +0900

    began refactoring of elevator-building dependency

--- src/buildings.py
+++ src/buildings.py
@@ -1,5 +1,6 @@
 from random import randint
-from sim_utils import print_status, randreq
+from sim_utils import print_status, rand_call
+from collections import deque
 
 
 class Building:
@@ -10,32 +11,83 @@ class Building:
         self.elevators = elevators
         self.num_elevators = len(elevators)
         for i in range(self.num_elevators):
-            self.elevators[i].set_id(i)
-        self.all_requests = []
- 
-    def make_reqs(self):
+            self.elevators[i].id = i
+        self.all_calls = deque()
+
+    def start_calls(self):
+        """ Start generating random elevator calls."""
+        pass
+
+    def generate_call(self):
+        """ Randomly generates an elevator call according to some distribution."""
+        pass
+
+    def select_elevator(self, call):
+        """ Judiciously selects an elevator to handle a generated call."""
+        pass
+
+    def process_call(self, call):
+        """ Tell selected elevator how to process the call."""
         pass
 
 
 class BasicBuilding(Building):
+    """
+    A building with a basic dispatcher.
+
+    Dispatches elevator calls according to basic elevator algorithm (SCAN):
+         1) While there are people in the elevator or people waiting in the
+             direction of the elevator, keep heading in that direction and
+             pickup/dropoff as necessary.
+         2) Once elevator has exhausted all requests in its current direction,
+             reverse direction and go to step 1) if there are requests. Else, stop
+             and wait for a call (or potentially move to another floor deemed more
+             effective)
+    """
+
     def __init__(self, floors, elevators):
         super().__init__(floors, elevators)
 
     def set_env(self, env):
         self.env = env
-        self.start = env.process(self.make_reqs())
+        self.start = env.process(self.generate_calls())
 
-    def make_reqs(self):
+    def generate_calls(self):
         while True:
-            yield self.env.timeout(randint(1, 2))
-            request = randreq(self.env.now, self.floors)    
+            yield self.env.timeout(randint(30, 30))
+            call = rand_call(self.env.now, self.floors)
             print_status(self.env.now,
-                    f'[Generate] Req {request.id}: floor {request.origin} to {request.dest}')
-            self.all_requests.append(request)
-            self.assign_req(request, randint(0, self.num_elevators - 1))
+                         f'[Generate] call {call.id}: floor {call.origin} to {call.dest}')
+            self.handle_call(call)
 
-    def assign_req(self, request, elevator_index):
+            self.all_calls.append(call)
+
+            # while True:
+            # if there exists a valid elevator:
+
+            self.assign_call(call, randint(0, self.num_elevators - 1))
+
+    # Copied over from elevators.py
+    def handle_call(self, call):
         print_status(self.env.now,
-                f'[Assign] Req {request.id}: Elevator {elevator_index}')    
-        self.elevators[elevator_index].handle_req(request)
+                     f'[Confirm] call {call.id}: Elevator {self.id} at floor {self.position}')
+        self.move_to(call.origin)
+        self.pick_up()
+        call.wait_time = self.env.now - call.orig_time
 
+        print_status(self.env.now, f'call {call.id} waited {call.wait_time / 10} s')
+        self.move_to(call.dest)
+        self.drop_off()
+        call.done = True
+        call.process_time = self.env.now - call.orig_time
+        print_status(self.env.now, f'[Done] call {call.id}')
+
+    # REFACTOR
+    def dispatch_calls(self):
+        with self.elevators[elevator_index].request() as request:
+            pass
+
+    def assign_call(self, call, elevator_index):
+        print_status(self.env.now,
+                     f'[Assign] call {call.id}: Elevator {elevator_index}')
+        self.elevators[elevator_index].handle_call(call)
--- src/elevators.py
+++ src/elevators.py
@@ -1,42 +1,23 @@
+import sys
 import simpy
 from sim_utils import print_status
 
 
 class Elevator:
-    def __init__(self):
+    def __init__(self, capacity):
         self.env = None
+        self.cart = None
         self.id = None
         self.position = 1
         self.dest = None
-        self.movement = 0   # 0: stationary, 1: up, -1: down
-        self.max_capacity = 0
+        self.movement = 0  # 0: stationary, 1: up, -1: down
+        self.direction = 0  # 0: idle, 1: servicing up, 2: servicing down
+        self.max_capacity = capacity
         self.curr_capacity = 0
         self.pickup_duration = 70
         self.dropoff_duration = 70
-       
-    def set_env(self, env):
-        self.env = env
-
-    def set_id(self, id):
-        self.id = id
-
-class BasicElevator(Elevator):
-    def __init__(self):
-        super().__init__()
-        self.max_capacity = 10
-        self.f2f_time = 100     # 10 seconds
-
-    def handle_req(self, request):
-        print_status(self.env.now, 
-                f'[Confirm] Req {request.id}: Elevator {self.id} at floor {self.position}') 
-        self.move_to(request.origin)
-        self.pick_up()
-        request.wait_time = self.env.now - request.time
-        print_status(self.env.now, f'Req {request.id} waited {request.wait_time / 10} s')
-        self.move_to(request.dest)
-        self.drop_off()
-        request.done = True
-        print_status(self.env.now, f'[Done] Req {request.id}')
+        self.max_capacity = capacity
+        self.f2f_time = 100  # 10 seconds
 
     def move_to(self, target_floor):
         if target_floor > self.position:
@@ -49,19 +30,22 @@ class BasicElevator(Elevator):
         self.movement = 0
 
     def move_one_floor(self):
-        yield self.env.timeout(self.f2f_time)
+        try:
+            yield self.env.timeout(self.f2f_time)
+        except simpy.Interrupt:
+            print("Elevator movement was interrupted. Exiting for now...")
+            sys.exit()
 
     def pick_up(self):
         if self.curr_capacity >= self.max_capacity:
             raise Exception('Elevator capacity exceeded. Dispatcher gotta git gud')
         self.curr_capacity += 1
         print_status(self.env.now,
-                f'(pick up) Elevator {self.id} at floor {self.position}, capacity now {self.curr_capacity}')
+                     f'(pick up) Elevator {self.id} at floor {self.position}, capacity now {self.curr_capacity}')
 
     def drop_off(self):
         if self.curr_capacity == 0:
             raise Exception('Nobody on elevator to drop off')
         self.curr_capacity -= 1
         print_status(self.env.now,
-                f'(drop off) Elevator {self.id} at floor {self.position}, capacity now {self.curr_capacity}')
-
+                     f'(drop off) Elevator {self.id} at floor {self.position}, capacity now {self.curr_capacity}')
