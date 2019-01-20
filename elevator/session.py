3e0ed4320d35b4a62c4851a23e1fbe66990301e9
shoyo-inokuchi <shoyoinokuchi@gmail.com>
2019-01-21 00:54:19 +0900

    added documentation for session

--- elevator/session.py
+++ elevator/session.py
@@ -1,26 +1,34 @@
 class Session:
-    """A session that runs a simulation and outputs results. """
+    """A wrapper for the SimPy library for simulation execution.
+
+    A session runs a simulation for a given building containing elevators
+    and outputs the corresponding results.
+    """
     def __init__(self, building, runtime=36000):
+        """Create a new simulation session for a given building.
+
+        env      -- simpy.Environment instance that runs the simulation
+        building -- buildings.Building subclass instance for conducting the
+                    simulation
+        runtime  -- total time* for running the simulation
+        (*Unit is 0.1 seconds. Example: 75 -> 7.5 in-simulation seconds)
+        """
         self.env = building.env
         self.building = building
         self.total_runtime = runtime
 
     def run(self):
-        if not self._valid_session():
-            raise Exception("Session was not valid. Could not run Session.")
-        else:
-            print("BEGINNING SESSION")
-            print("=================")
-            self.env.run(until=self.total_runtime)
-            print("=================")
-            print("ENDING SESSION")
-            # print("\nRESULTS:")
-            # self._disp_metrics()
-
-    def _valid_session(self):
-        return self.env and self.building and self.total_runtime
+        """Run the session."""
+        print("BEGINNING SESSION")
+        print("=================")
+        self.env.run(until=self.total_runtime)
+        print("=================")
+        print("ENDING SESSION")
+        print("\nRESULTS:")
+        self._disp_metrics()
 
     def _disp_metrics(self):
+        """Calculate and print simulation results."""
         # TODO: Maybe make this more efficient
         total_wait = sum(r.wait_time for r in self.building.call_history if
                          r.done)
